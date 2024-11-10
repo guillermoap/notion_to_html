66db1bb47c907dbf4d7d80f61216fb3951c8fbe3
Guillermo Aguirre <guillermoaguirre@hey.com>
2024-11-10 15:19:47 -0300

    Add timeout configs (#5)
    * Add timeout configs
    
    * Bump to version 1.1.4

--- lib/notion_to_html.rb
+++ lib/notion_to_html.rb
@@ -19,6 +19,30 @@ module NotionToHtml
   #   @return [String] The database ID in Notion that the module will interact with.
   setting :notion_database_id
 
+  # @!attribute [rw] notion_timeout
+  #   @return [Integer] The number of seconds to wait for a response from the Notion API before timing out.
+  #   @example
+  #     config.notion_timeout = 60 # Wait up to 60 seconds for API responses
+  setting :notion_timeout, default: 30
+
+  # @!attribute [rw] notion_default_page_size
+  #   @return [Integer] The default number of records to return per page when making paginated requests.
+  #   @example
+  #     config.notion_default_page_size = 50 # Return 50 records per page
+  setting :notion_default_page_size, default: 100
+
+  # @!attribute [rw] notion_default_max_retries
+  #   @return [Integer] The maximum number of times to retry failed API requests.
+  #   @example
+  #     config.notion_default_max_retries = 3 # Retry failed requests up to 3 times
+  setting :notion_default_max_retries, default: 5
+
+  # @!attribute [rw] notion_default_retry_after
+  #   @return [Integer] The number of seconds to wait between retry attempts for failed API requests.
+  #   @example
+  #     config.notion_default_retry_after = 5 # Wait 5 seconds between retries
+  setting :notion_default_retry_after, default: 10
+
   # @!attribute [rw] cache_store
   #   @return [ActiveSupport::Cache::Store] The cache store used to cache responses from the Notion API.
   #   @default ActiveSupport::Cache::MemoryStore.new
